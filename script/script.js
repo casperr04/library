bfeffc0d540bcf5ed2a3db92e09031d98672a12d
casperr04 <kacperjaroslawurbanczyk@gmail.com>
2024-06-20 21:56:21 +0200

    Refactor and changes

--- script/script.js
+++ script/script.js
@@ -3,74 +3,28 @@ const library = {
 };
 
 function getFormObject(formData){
-    let formObject = {};
+    let book = {};
     formData.forEach((value, key) => {
-        formObject[key] = value;
+        book[key] = value;
       });
-    return formObject;
+    return book;
 }
 
 
-const form = document.querySelector("#form")
-const div = document.querySelector(".book-div")
+const form = document.querySelector("#form");
+const div = document.querySelector(".book-div");
 let bookId = 0;
 
 class Book {
-    constructor(author, title, isRead, id) {
+    constructor(author, title, isRead, id, pages) {
       this.author = author;
       this.title = title;
       this.isRead = isRead;
       this.id = id;
+      this.pages = pages;
   }
 }
 
-
-function addTitle(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const formObject = getFormObject(formData);
-
-    if(formObject["author"] == "" || formObject["title"] == "") {
-        return;
-    }
-    const content = document.createElement("div");
-    content.classList.add("book-display");
-    content.innerHTML = `
-        <div class="book-info-base">
-            Title:
-        </div>
-        <div class="book-info-element">
-            ${formObject["title"]}
-        </div>
-        <div class="book-info-base">
-            Author:
-        </div>
-        <div class="book-info-element">
-            ${formObject["author"]}
-        </div>
-        <div>
-            <button type="button" class="delete-button" id="delete-button-${bookId}">Delete</button>
-            <button type="button" class="read-button" id="read-button-${bookId}">Toggle Read</button>
-        </div>
-        `;
-
-    div.appendChild(content);
-
-    const deleteButton = document.querySelector(`#delete-button-${bookId}`);
-    deleteButton.addEventListener("click", deleteTitle);
-
-    const readButton = document.querySelector(`#read-button-${bookId}`);
-    readButton.addEventListener("click", setTitleToRead)
-
-    formObject.id = bookId;
-    formObject.isRead = false;
-
-    library.books.push(formObject);
-
-    bookId++;
-    form.reset()
-}
 
 function populateTitle(book) {
 
@@ -78,76 +32,130 @@ function populateTitle(book) {
         return;
     }
 
-    let currentBookId = book.id
+    let currentBookId = book.id;
+    const bookDisplay = document.createElement('div');
 
     const content = document.createElement("div");
+    let readStatus = "Read";
+    let readClass=  "read";
 
-    if(book.isRead){
-        content.classList.add("book-display");
+    if(!book.isRead){
+        bookDisplay.classList.add("book-display");
+        readStatus = "Not Read";
+        readClass = "not-read";
     } else {
-        content.classList.add("book-display-read")
+        bookDisplay.classList.add("book-display-read");
     }
 
-    content.innerHTML = `
-        <div class="book-info-base">
-            Title:
-        </div>
-        <div class="book-info-element">
-            ${book["title"]}
-        </div>
-        <div class="book-info-base">
-            Author:
-        </div>
-        <div class="book-info-element">
-            ${book["author"]}
-        </div>
-        <div>
-            <button type="button" class="delete-button" id="delete-button-${currentBookId}">Delete</button>
-            <button type="button" class="read-button" id="read-button-${currentBookId}">Toggle Read</button>
-        </div>
-        `;
+    const base1 = document.createElement('div');
+    const base2 = document.createElement('div');
+    const base3 = document.createElement('div');
+    const element1 = document.createElement('div');
+    const element2 = document.createElement('div');
+    const element3 = document.createElement('div');
+    
+    base1.classList.add("book-info-base");
+    base2.classList.add("book-info-base");
+    base3.classList.add("book-info-base");
 
-    div.appendChild(content);
+    element1.classList.add("book-info-element");
+    element2.classList.add("book-info-element");
+    element3.classList.add("book-info-element");
 
-    const deleteButton = document.querySelector(`#delete-button-${currentBookId}`);
+    base1.textContent = "Title:";
+    base2.textContent = "Author:";
+    base3.textContent = "Pages:";
+
+    element1.textContent = book["title"];
+    element2.textContent = book["author"];
+    element3.textContent = book["pages"];
+
+    const readDiv = document.createElement('div');
+    readDiv.classList.add(readClass);
+    readDiv.textContent = readStatus;
+
+    const deleteButton = document.createElement('button');
+    const readButton = document.createElement('button');
+
+    deleteButton.id = `delete-button-${currentBookId}`;
+    readButton.id = `read-button-${currentBookId}`;
+
+    deleteButton.classList.add("delete-button");
+    readButton.classList.add("read-button");
+
+    deleteButton.textContent = " Delete ";
+    readButton.textContent = " Toggle Read ";
+
+    const buttonDiv = document.createElement('div');
+
+    buttonDiv.appendChild(deleteButton);
+    buttonDiv.appendChild(readButton);
+
+
+    bookDisplay.appendChild(base1);
+    bookDisplay.appendChild(element1);
+    bookDisplay.appendChild(base2);
+    bookDisplay.appendChild(element2);
+    bookDisplay.appendChild(base3);
+    bookDisplay.appendChild(element3)
+    bookDisplay.appendChild(readDiv);
+    bookDisplay.appendChild(buttonDiv);
+
+    div.append(bookDisplay);
     deleteButton.addEventListener("click", deleteTitle);
+    readButton.addEventListener("click", setTitleToRead);
 
-    const readButton = document.querySelector(`#read-button-${currentBookId}`);
-    readButton.addEventListener("click", setTitleToRead)
-
-    bookId = currentBookId + 1
-    form.reset()
+    bookId = currentBookId + 1;
+    form.reset();
 }
 
+function addTitle(event) {
+    event.preventDefault();
 
+    const formData = new FormData(event.target);
+    const formObject = getFormObject(formData);
+    console.log(formObject)
+
+    const isRead = formObject.isRead == "on" ? true : false;
+
+    const book = new Book(formObject.author, formObject.title, isRead, bookId, formObject.pages)
+    library.books.push(book);
+
+    bookId++;
+    displayTitles(library);
+}
 
 function setTitleToRead(event) {
-    const parent = event.target.parentNode.parentNode;
-    parent.classList.toggle("book-display-read");
-    parent.classList.toggle("book-display");
+    const myBookId = parseInt(event.target.id.match(/\d+$/)[0], 10);
+    const bookRefIndex = library.books.findIndex(book => book.id == myBookId)
+
+    if(library.books[bookRefIndex].isRead){
+        library.books[bookRefIndex].isRead = false;
+    } else {
+        library.books[bookRefIndex].isRead = true;
+    }
+    displayTitles(library);
 }
 
 
 
 function deleteTitle(event){
-    const element = event.target;
-    const parent = element.parentNode.parentNode;
-    parent.remove();
+    const myBookId = parseInt(event.target.id.match(/\d+$/)[0], 10);
+    library.books = library.books.filter(filterBook => !(filterBook.id == myBookId));
+    displayTitles(library);
 }
-
 
 form.addEventListener("submit", addTitle);
 
 function displayTitles(library){
+    div.innerHTML = ''
     library.books.forEach(book => populateTitle(book));
 }
 
-const book1 = new Book("Jimmy John", "How to eat rocks", false, 1);
-const book2 = new Book("Johnagle Books", "DIY Sand tutorial", true, 2);
+const book1 = new Book("Jimmy John", "How to eat rocks", false, 1, 25);
+const book2 = new Book("Johnagle Books", "DIY Sand tutorial", true, 2, 37);
 
-
-library.books.push(book1)
-library.books.push(book2)
-
+library.books.push(book1);
+library.books.push(book2);
 
 displayTitles(library);
